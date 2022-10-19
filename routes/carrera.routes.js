17978bb95452efea611be07218101323e387c4ec
Jim Loza <jimloza25@gmail.com>
2022-10-18 19:34:46 -0500

    validaciones simples

--- routes/carrera.routes.js
+++ routes/carrera.routes.js
@@ -1,8 +1,18 @@
-const { Router } = require('express')
-const { store_carrera } = require('../controllers/carrera.controller')
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { store_carrera } = require('../controllers/carrera.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+
 
 const router = Router();
 
-router.post('/', store_carrera)
+router.post('/', [
+    check('Nombre', 'El campo nombre debe de ser de tipo string').isString(),
+    check('Nombre', 'El campo nombre no contiene informacion').notEmpty(),
+    check('Duracion', 'El campo duracion debe de ser de tipo numerico').isNumeric(),
+    check('Duracion', 'El campo duracion no contiene informacion').notEmpty(),
+    validarCampos
+], store_carrera)
 
 module.exports = router;
